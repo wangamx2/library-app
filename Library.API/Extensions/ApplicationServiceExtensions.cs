a3cd7df4fcdb30e3fe790ea25ecda3d6371a1e1b
Wanga Mxuthu <waramx2@gmail.com>
2024-06-11 09:52:57 +0200

    build

--- Library.API/Extensions/ApplicationServiceExtensions.cs
+++ Library.API/Extensions/ApplicationServiceExtensions.cs
@@ -11,9 +11,35 @@ namespace Library.API.Extensions
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
         {
-            services.AddDbContext<DataContext>(opt =>
+            services.AddDbContext<DataContext>(options =>
             {
-                opt.UseNpgsql(config.GetConnectionString("DefaultConnection"));
+                var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+                string connStr;
+                if (env == "Development")
+                {
+                    connStr = config.GetConnectionString("DefaultConnection");
+                }
+                else
+                {
+                    // Use connection string provided at runtime by Flyio.
+                    var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+                    // Parse connection URL to connection string for Npgsql
+                    connUrl = connUrl.Replace("postgres://", string.Empty);
+                    var pgUserPass = connUrl.Split("@")[0];
+                    var pgHostPortDb = connUrl.Split("@")[1];
+                    var pgHostPort = pgHostPortDb.Split("/")[0];
+                    var pgDb = pgHostPortDb.Split("/")[1];
+                    var pgUser = pgUserPass.Split(":")[0];
+                    var pgPass = pgUserPass.Split(":")[1];
+                    var pgHost = pgHostPort.Split(":")[0];
+                    var pgPort = pgHostPort.Split(":")[1];
+
+                    connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+                }
+
+                options.UseNpgsql(connStr);
             });
 
             services.AddCors(opt => {
